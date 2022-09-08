79624dd4909c88761be88aee7ae92aab452aef29
Phyllis Wong <wong.phyllism@gmail.com>
2022-09-08 12:38:07 -0600

    feat: mmove category list to directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
-
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
   const categories = [
@@ -37,11 +35,7 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
-      { categories.map(category => 
-        <CategoryItem category={category} key={category.id} />
-      )}
-    </div>
+    <Directory categories={categories} />
   );
 }
 
