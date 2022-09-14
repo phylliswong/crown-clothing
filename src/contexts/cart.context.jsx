92444308e96ff9b80a074e813c0e141a0710a37d
Phyllis Wong <wong.phyllism@gmail.com>
2022-09-13 19:43:40 -0600

    feat: add cart count to context and cart-icon

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,8 @@
-import { createContext, useState } from "react";
+import {
+  createContext,
+  useEffect,
+  useState,
+} from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   const exsistingCartItem = cartItems.find((cartItem) => 
@@ -20,17 +24,32 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addCartItem: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (subTotal, cartItem) => subTotal + cartItem.quantity, 0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   }
 
-  const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
 
   return (
     <CartContext.Provider value={value}>
