ac8b2f2823d718ac1a36cdae3e8027950687eaba
Phyllis Wong <wong.phyllism@gmail.com>
2022-09-15 17:16:44 -0600

    refactor: category preview feat: add category component and nested routes wip: category and shop styles conflict

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,16 @@
-import { useContext, Fragment } from "react";
-import { Outlet } from "react-router-dom";
-import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 import './shop.styles.scss';
 
 const Shop = () => {
+
   return (
-    <Fragment>
-      <CategoryPreview />
-      <Outlet />
-    </Fragment>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   )
 }
 
