4fdf71763c5d4edaf47667e7c245d2ddee898010
Phyllis Wong <wong.phyllism@gmail.com>
2022-09-20 12:44:07 -0600

    feat: add redux user reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import { Outlet, Routes, Route } from "react-router-dom";
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
+
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Checkout from "./routes/checkout/checkout.component";
 import Home from "./routes/home/home.component";
 import Shop from "./routes/shop/shop.component";
+
+import { setCurrentUser } from "./store/user/user.action";
 
 
 const Contact = () => {
@@ -16,6 +27,19 @@ const Contact = () => {
 }
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      };
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />} >
