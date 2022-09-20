a1e3b2d7c75abdb3f6331dba8c8811879840ca57
Phyllis Wong <wong.phyllism@gmail.com>
2022-09-20 13:03:27 -0600

    feat: redux user reducer (#21)
    * feat: add redux user reducer
    * feat: add selector and convert user context into redux selector

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+
 import App from './App';
 import { CartProvider } from './contexts/cart.context';
 import { CategoriesProvider } from './contexts/categories.context';
-import { UserProvider } from './contexts/user.context';
 import { store } from './store/store';
 
 import './index.scss';
@@ -19,13 +19,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,15 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { selectCurrentUser } from "../../store/user/user.selector";
+
 import { ReactComponent as CrwnSvg } from "../../assets/crown.svg";
 import { CartContext } from "../../contexts/cart.context";
-import { UserContext } from "../../contexts/user.context";
-import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { LogoContainer,
   NavigationContainer,
   NavLinks,
@@ -15,7 +19,8 @@ import { LogoContainer,
 // import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
+
   const { isCartOpen } = useContext(CartContext);
 
   return (
